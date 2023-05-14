74e238ff9ef743a721db7ad347ddd30a4f9dc4ec
Jorge Machuca <jmachuca06@gmail.com>
2023-05-13 20:35:54 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "./food-order/",
+    base: "/food-order/",
     build: {
-        outDir: "/dist",
+        outDir: "./dist",
     },
 });
